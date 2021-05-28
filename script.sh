ec822bbcc452e6c369080d50a9618df50e759791
BRiera <baptiste.riera@outlook.com>
2021-05-28 16:27:04 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,14 @@ apt -y update
 apt -y full-upgrade
 
 #
+# Configure ssh
+#
+
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sed -i "s|#Port.*|Port 91|gi" /etc/ssh/sshd_config
+sed -i "s|PermitRootLogin yes|PermitRootLogin no|gi" /etc/ssh/sshd_config
+
+#
 # Install Sudo
 #
 
